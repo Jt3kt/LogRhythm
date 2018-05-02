c7893f234f9159c21581dc245adb72fd4e60ef3a
Jtekt <github.jtekt@gmail.com>
2018-05-02 15:28:11 -0400

    Fix Powershell Version requirement.

--- Sysmon_Deploy/lr_installagent.ps1
+++ Sysmon_Deploy/lr_installagent.ps1
@@ -1,4 +1,4 @@
-#Requires -Version 5.0
+#Requires -Version 5.1
 #Requires -RunAsAdministrator
 #This script is designed to uninstall the LogRhythm System Monitor Agent silently from an endpoint domain joined Windows device.
 #
