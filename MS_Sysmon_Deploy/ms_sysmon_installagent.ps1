432415e800ab5802e35e21d6edb404e55eaf05e2
Jtekt <github.jtekt@gmail.com>
2018-05-14 13:15:28 -0400

    Added update config function.  Broke out copyFIle function.

--- MS_Sysmon_Deploy/ms_sysmon_installagent.ps1
+++ MS_Sysmon_Deploy/ms_sysmon_installagent.ps1
@@ -16,17 +16,23 @@
 #
 # Sha256 hashes are genearted prior to transmitting to target host.    
 #
+# The copyFile function has been written to be standalone to support portability.
+#
 # Written by Jtekt 07 May 2018 
 # https://github.com/Jtekt/LogRhythm
 # Version 0.5
 # Sample usage: 
-# ms_sysmon_installagent.ps1 -computer COMPUTERNAME -srcConfig CONFIGNAME -installStagePath C:\temp\sysmon
+#  Install and apply client configuration
+# ms_sysmon_installagent.ps1 -computer COMPUTERNAME -srcConfig CONFIGNAME -installStagePath C:\temp\ms_sysmon 
+#  Update client configuration
+# ms_sysmon_installagent.ps1 -computer COMPUTERNAME -srcConfig CONFIGNAME -installStagePath C:\temp\ms_sysmon -force true
 #
 param (
     [Parameter(Mandatory=$true,Position=1)][string]$computer,
     [Parameter(Mandatory=$false,Position=2)][string]$srcConfig = 'sysmonconfig-export.xml',
     [Parameter(Mandatory=$false,Position=3)][string]$installStagePath = "C:\temp\ms_sysmon",
-    [Parameter(Mandatory=$false,Position=4)][string]$installSys64 = $false
+    [Parameter(Mandatory=$false,Position=4)][string]$installSys64 = $false,
+    [Parameter(Mandatory=$false,Position=5)][string]$force = $false
 )
 #$fileSource = local installation file source folder.  Must be set to in order to operate.
 $Global:fileSource = "C:\temp\ms_sysmon\"
@@ -42,6 +48,13 @@ $Global:installStatus = $false
 $Global:OS_Arch = $null
 $installStagePath = $installStagePath + "\"
 $Global:installArguments = ' -accepteula -i '+$installStagePath+$srcConfig
+
+trap [Exception] {
+    write-error $("Exception: " + $_)
+    exit 1
+}
+
+if($debugMode -eq 1){$DebugPreference = "Continue"}else{$DebugPreference = "SilentlyContinue"}
 
 #Identifies source installation files and generates source hashes for file validation.
 Function identifyInstallFIle {
@@ -81,111 +94,138 @@ Function identifyInstallFIle {
     $Global:srcFileHash = Get-FileHash $Global:fileSource$srcConfig -Algorithm SHA256
 }
 
-#Function is responsible for copying source files to destination host
-Function copyApp{
-    #Function variables used for file validation.
-    $fullPath = $installStagePath+$Global:installFileName
-    $fullPath2 = $installStagePath+$srcConfig
+Function copyFile{
+    Param(
+        [Parameter(Mandatory=$true, position=1)]
+        $filename,
+        [Parameter(Mandatory=$true, position=2)]
+        $target,
+        [Parameter(Mandatory=$true, position=3)]
+        $srcPath,
+        [Parameter(Mandatory=$true, position=5)]
+        $destPath,
+        [Parameter(Mandatory=$false, position=6)]
+        $fileHash,
+        [Parameter(Mandatory=$false, position=7)]
+        $fileForce
+    )
+    $fullPath = $destPath+$filename
     #Establish connection to target.
     try{ 
-        $Session = New-PSSession -ComputerName $computer
+        $Session = New-PSSession -ComputerName $target -ErrorAction Stop
     }
     catch{
-        Write-host "Unable to establish New-PSSession to $computer.`n$($Error[0].Exception.Message)"
+        Write-host "Unable to establish New-PSSession to $target.`n$($Error[0].Exception.Message)"
         exit 1
-        #continue
     }
     #Begin existing file/folder structure check & file verification.
     #Verify target directory
     try{ 
-        $dirstatus = Invoke-Command -ComputerName $computer -scriptBlock {Test-Path $args -PathType Container} -ArgumentList $installStagePath -ErrorAction Stop
+        $dirstatus = Invoke-Command -ComputerName $target -scriptBlock {Test-Path $args -PathType Container} -ArgumentList $destPath -ErrorAction Stop
     }
     catch{
         Write-host "Error while attempting to verify installation directory.`n$($Error[0].Exception.Message)"
         exit 1
-        #continue
     }
     if ($dirstatus -eq $false) {
-        Write-Verbose "Creating target directory $installStagePath on $computer."
+        Write-Verbose "Creating target directory $destPath on $target."
         try{ 
-            Invoke-Command -ComputerName $computer -scriptBlock {New-Item -Path $args -type Directory -Force} -ArgumentList $installStagePath -ErrorAction Stop
+            Invoke-Command -ComputerName $target -scriptBlock {New-Item -Path $args -type Directory -Force} -ArgumentList $destPath -ErrorAction Stop
 			Start-Sleep .5
         }
         catch{
-            Write-host "Error while attempting to create directory $installStagePath.`n$($Error[0].Exception.Message)"
+            Write-host "Error while attempting to create directory $destPath.`n$($Error[0].Exception.Message)"
             exit 1
-            #continue
         }
     }
     else{
         Write-Verbose "Directory existed.  Checking for install file."
         Write-Verbose "Checking for $fullPath"
         try{ 
-            $filestatus = Invoke-Command -ComputerName $computer -scriptBlock {Test-Path $args[0] -PathType Leaf} -ArgumentList $fullPath -ErrorAction Stop
+            $filestatus = Invoke-Command -ComputerName $target -scriptBlock {Test-Path $args[0] -PathType Leaf} -ArgumentList $fullPath -ErrorAction Stop
         }
         catch{
             Write-host "Error while checking for $fullPath.`n$($Error[0].Exception.Message)"
             exit 1
-            #continue
         }
         Write-Verbose "File status is $filestatus."    
     }
     if ($filestatus -eq $true){
-        Write-Verbose "$installFileName found on $computer under $installStagePath."
+        Write-Verbose "$filename found on $target under $destPath."
+        try{
+            if ($fileForce -eq $true){
+                #Begin verifying remote hash
+                try{ 
+                    $remoteOrigHash = Invoke-Command -ComputerName $target -scriptBlock {Get-FileHash $args -Algorithm SHA256} -ArgumentList $fullPath -ErrorAction Stop
+                }
+                catch{
+                    Write-host "Error while attempting to retrieve sha256 hash for $fullPath.`n$($Error[0].Exception.Message)"
+                    exit 1
+                }
+                Copy-Item -Path $srcPath$filename -Destination $destPath$filename -ToSession $session -ErrorAction Stop -Force
+                Start-Sleep 8
+                try{ 
+                    $remoteFileHash = Invoke-Command -ComputerName $target -scriptBlock {Get-FileHash $args -Algorithm SHA256} -ArgumentList $fullPath -ErrorAction Stop
+                }
+                catch{
+                    Write-host "Error while attempting to retrieve sha256 hash for $fullPath.`n$($Error[0].Exception.Message)"
+                    exit 1
+                }
+                #Verify destination file has been altered.
+                if ($remoteOrigHash -ne $remoteFileHash){
+                    #Verify origin file hash exists.
+                    if ($fileHash){
+                        #Compare origin file hash vs updated destination file.
+                        if ($fileHash.hash -eq $remoteFileHash.hash){
+                            Write-Host "Copied $filename and hash verified."
+                            $Global:copyStatus = $true
+                        }
+                        else{
+                            Write-Host "Copied $filename hash mismatch.  Cleanup target destination folder and re-run."
+                            $Global:copyStatus = $false
+                            exit 1
+                        }
+                    }
+                }
+            } 
+        }
+        catch{
+            Write-host "Error while attempting to copy $filename to $destPath.`n$($Error[0].Exception.Message)"
+            exit 1
+        }
     }
     else {
-        try{ 
-            Copy-Item -Path $Global:fileSource$installFileName -Destination $installStagePath$installFileName -ToSession $session -ErrorAction Stop
+        try{
+            Copy-Item -Path $srcPath$filename -Destination $destPath$filename -ToSession $session -ErrorAction Stop
             Start-Sleep 8
         }
         catch{
-            Write-host "Error while attempting to copy $installFileName to $installStagePath.`n$($Error[0].Exception.Message)"
+            Write-host "Error while attempting to copy $filename to $destPath.`n$($Error[0].Exception.Message)"
             exit 1
         }
+        #Begin verifying remote hash
         try{ 
-            Copy-Item -Path $Global:fileSource$srcConfig -Destination $installStagePath$srcConfig -ToSession $session -ErrorAction Stop
-            STart-Sleep 1
+            $remoteFileHash = Invoke-Command -ComputerName $target -scriptBlock {Get-FileHash $args -Algorithm SHA256} -ArgumentList $fullPath -ErrorAction Stop
         }
         catch{
-            Write-host "Error while attempting to copy $srcConfig to $installStagePath.`n$($Error[0].Exception.Message)"
+            Write-host "Error while attempting to retrieve sha256 hash for $fullPath.`n$($Error[0].Exception.Message)"
             exit 1
         }
-    }
-    #Begin verifying remote hash
-    try{ 
-        $remoteFileHash = Invoke-Command -ComputerName $computer -scriptBlock {Get-FileHash $args -Algorithm SHA256} -ArgumentList $fullPath -ErrorAction Stop
-    }
-    catch{
-        Write-host "Error while attempting to retrieve sha256 hash for $fullPath.`n$($Error[0].Exception.Message)"
-        exit 1
-    }
-    try{ 
-        $remoteFileHash2 = Invoke-Command -ComputerName $computer -scriptBlock {Get-FileHash $args -Algorithm SHA256} -ArgumentList $fullPath2 -ErrorAction Stop
-    }
-    catch{
-        Write-host "Error while attempting to retrieve sha256 hash for $fullPath2.`n$($Error[0].Exception.Message)"
-        exit 1
-    }
-    if ($Global:installFileHash.hash -eq $remoteFileHash.hash){
-        Write-Verbose "Copied $installFileName hash verified."
-        $Global:copyStatus = $true
-    }
-    else{
-        Write-Host "Copied $installFileName hash mismatch.  Cleanup target destination folder and re-run."
-        $Global:copyStatus = $false
-        exit 1
-    }
-    if ($Global:srcFileHash.hash -eq $remoteFileHash2.hash){
-        Write-Verbose "Copied $srcConfig hash verified."
-        Write-Host "File copy complete.  Proceeding to install."
-        $Global:copyStatus = $true
-    }
-    else{
-        Write-Host "Copied $srcConfig hash mismatch.  Cleanup target destination folder and re-run."
-        $Global:copyStatus = $false
-        exit 1
+        if ($fileHash){
+            if ($fileHash.hash -eq $remoteFileHash.hash){
+                Write-Host "Copied $filename and hash verified."
+                $Global:copyStatus = $true
+            }
+            else{
+                Write-Host "Copied $filename hash mismatch.  Cleanup target destination folder and re-run.`nIf attempting to update client config add -force true to command."
+                $Global:copyStatus = $false
+                exit 1
+            }
+        }
     }
 }
+
+
 
 #Function is responsible for executing the MS Sysmon install silently.
 Function installApp {
@@ -194,31 +234,60 @@ Function installApp {
         Write-Host $Global:serviceName" is already installed."
     }
     else {
-        copyApp
         if ($Global:copyStatus -eq $true)
         {
             Write-Verbose "Install initiating."
             try{ 
-                $newproc = Invoke-WmiMethod -class Win32_process -name Create -ArgumentList ("cmd.exe /C start /MIN "+$installStagePath+$installFileName+$installArguments) -ComputerName $computer 
-                #Write-Verbose "Invoke-WmiMethod -class Win32_process -name Create -ArgumentList (cmd.exe /C start /MIN +$installStagePath+$installFileName+$installArguments) -ComputerName $computer"
+                $newproc = Invoke-WmiMethod -class Win32_process -name Create -ArgumentList ("cmd.exe /C start /MIN "+$installStagePath+$installFileName+$installArguments) -ComputerName $computer -ErrorAction Stop
                 Write-Verbose $installStagePath$installFileName$installArguments
             }
             catch{
                 Write-host "Error while attempting kick off installation process.`n$($Error[0].Exception.Message)"
                 exit 1
-                #continue
             }
             if ($newproc.ReturnValue -eq 0 ) 
             { 
                 Write-Verbose "Command invoked sucessfully." 
                 #This sleep may need to be tuned for your environment based on maximum time observed installing on target hosts.
-                Start-Sleep -Seconds 10
+                Start-Sleep 1
                 $Global:installStatus = $true
             }
         }
         elseif ( $Global:copyStatus -eq $false ) 
         {
             Write-Host "File copy interrupted.  Aborting install."
+            Exit 1
+        }
+    }
+}
+
+#Function is responsible for updating MS Sysmon config.
+Function updateConfig {
+    if ($Global:installStatus -eq $true)
+    {
+        if ($Global:copyStatus -eq $true)
+        {
+            Write-Host "Update config initiating."
+            try{ 
+                $newproc = Invoke-WmiMethod -class Win32_process -name Create -ArgumentList ("cmd.exe /C start /MIN "+$installStagePath+$installFileName+" -c "+$installStagePath+$srcConfig) -ComputerName $computer -ErrorAction Stop
+                Write-Verbose $installStagePath$installFileName" -c "$installStagePath$srcConfig
+            }
+            catch{
+                Write-host "Error while attempting kick off config update process.`n$($Error[0].Exception.Message)"
+                exit 1
+                #continue
+            }
+            if ($newproc.ReturnValue -eq 0 ) 
+            { 
+                Write-Host "Update config invoked sucessfully." 
+                #This sleep may need to be tuned for your environment based on maximum time observed installing on target hosts.
+                Start-Sleep 1
+                $Global:installStatus = $true
+            }
+        }
+        elseif ( $Global:copyStatus -eq $false ) 
+        {
+            Write-Host "File copy interrupted.  Aborting configuration update."
             Exit 1
         }
     }
@@ -237,7 +306,7 @@ Function serviceStatus {
     if ($servstat -like '*Running*'){
         $Global:serviceStatus = $true
         $Global:installStatus = $true
-        Write-Verbose $Global:serviceName" is running on "$computer"."
+        Write-Host $Global:serviceName" is running on $computer."
     }
     elseif ($servstat -like '*Stopped*') {
         $Global:serviceStatus = $false
@@ -322,10 +391,21 @@ Function serviceStart {
     }
 }
 
-
-
-identifyInstallFIle
 serviceStatus
-installApp
-serviceStart
+identifyInstallFIle
+Write-Verbose "file status is $Global:installStatus"
+if ($Global:installStatus -eq $true) {
+    #Update Sysmon configuration file.
+    copyFile $srcConfig $computer $Global:fileSource $installStagePath $Global:srcFileHash $force
+    updateConfig
+    if ($Global:serviceStatus = $false) {
+        serviceStart
+    }
+}
+elseif ($Global:installStatus -eq $false) {
+    copyFile $Global:installFileName $computer $Global:fileSource $installStagePath $Global:installFileHash $force
+    copyFile $srcConfig $computer $Global:fileSource $installStagePath $Global:srcFileHash $force
+    installApp
+    serviceStart
+}
 Exit 0
